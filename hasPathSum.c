9d590626965f47c9f1f1096b41bba4bf5e60755b
Atosi <atosidas44@gmail.com>
2023-07-09 22:27:50 +0530

    Complete hasPathSum

--- hasPathSum.c
+++ hasPathSum.c
@@ -1,3 +1,15 @@
 bool hasPathSum(struct TreeNode* root, int targetSum){
+    if (root == NULL) return false;
 
+    int newTargetSum = targetSum - root->val;
+
+    if (newTargetSum == 0 && root->left == NULL && root->right == NULL)
+        return true;
+
+    bool flag = false;
+
+    flag = flag || hasPathSum(root->left, newTargetSum);
+    flag = flag || hasPathSum(root->right, newTargetSum);
+
+    return flag;
 }
