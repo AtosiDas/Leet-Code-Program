24f9f16ea37f2dd8ebd4e5d3089f5d2d57391b1b
Atosi <atosidas44@gmail.com>
2023-07-11 16:56:54 +0530

    ProblemSolved

--- SearchInsertPosition.c
+++ SearchInsertPosition.c
@@ -4,5 +4,21 @@ If not, return the index where it would be if it were inserted in order.
 You must write an algorithm with O(log n) runtime complexity.
 
 int searchInsert(int* nums, int numsSize, int target){
-
+    int low = 0;
+    int high = numsSize - 1;
+    int mid;
+    while(low <= high) {
+        mid = (low + high)/2;
+        if(target < nums[mid]){
+            high = mid - 1;
+        }
+        else if(target == nums[mid]){
+            return mid;
+            break;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return (high + 1);
 }
