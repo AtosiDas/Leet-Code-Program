48f4fd2678d181073a7b0b030e3c2f8a51c03414
Atosi <atosidas44@gmail.com>
2023-07-11 16:55:18 +0530

    FunctionForTheProblem

--- SearchInsertPosition.c
+++ SearchInsertPosition.c
@@ -2,3 +2,7 @@ Given a sorted array of distinct integers and a target value, return the index i
 If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
+
+int searchInsert(int* nums, int numsSize, int target){
+
+}
