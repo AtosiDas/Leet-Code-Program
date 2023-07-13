ae137baf2a147bf15f6ff273bf8a7c3bd62bff50
Atosi <atosidas44@gmail.com>
2023-07-13 13:00:43 +0530

    InitialStatement

--- Remove_Duplicates_From_SortedList.c
+++ Remove_Duplicates_From_SortedList.c
@@ -1,4 +1,13 @@
 Given the head of a sorted linked list, delete all duplicates such that each element appears only
 once. Return the linked list sorted as well.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head){
 
+}
 
