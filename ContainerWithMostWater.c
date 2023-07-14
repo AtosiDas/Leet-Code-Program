b076cca25f54b249334c814e21f90c700404fca1
Atosi <atosidas44@gmail.com>
2023-07-14 20:29:40 +0530

    Problem Solved

--- ContainerWithMostWater.c
+++ ContainerWithMostWater.c
@@ -5,6 +5,30 @@ the container contains the most water.
 Return the maximum amount of water a container can store.
 Notice that you may not slant the container.
 
+int max(int a, int b);
+int min(int a, int b);
 int maxArea(int* height, int heightSize){
-
+    int area = 0;
+    int low = 0;
+    int high = heightSize - 1;
+    while(low < high) {
+        area = max(area, (high - low)*(min(height[low],height[high])));
+        if(height[low] < height[high])
+            low++;
+        else
+            high--;
+    }
+    return area;
+}
+int max(int a, int b){
+    if(a <= b)
+        return b;
+    else
+        return a;
+}
+int min(int a, int b){
+    if(a <= b)
+        return a;
+    else
+        return b;
 }
