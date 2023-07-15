d48b8e31fde97e71184d1d06de6651056bd8bce4
Atosi <atosidas44@gmail.com>
2023-07-15 13:50:33 +0530

    Solved

--- RemoveLinkedListElements.c
+++ RemoveLinkedListElements.c
@@ -8,5 +8,25 @@ that has Node.val == val, and return the new head.
  * };
  */
 struct ListNode* removeElements(struct ListNode* head, int val){
-
+    struct ListNode* temp = head;
+    struct ListNode* curr = temp;
+    while(temp != NULL){
+        while(temp != NULL && temp->val == val){
+            if(curr == temp){
+                head = temp ->next;
+                temp = head;
+                curr = temp;
+            }
+            else {
+                curr->next = temp->next;
+                temp = curr->next;
+            }
+        }
+        curr = temp;
+        if(temp != NULL)
+            temp = temp->next;
+        else
+            break;
+    }
+    return head;
 }
