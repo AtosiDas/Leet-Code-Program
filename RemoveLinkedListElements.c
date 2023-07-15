dfff2a8e23447f064ad498384f31c2d78912a700
Atosi <atosidas44@gmail.com>
2023-07-15 13:48:29 +0530

    Initial Statement

--- RemoveLinkedListElements.c
+++ RemoveLinkedListElements.c
@@ -1,2 +1,12 @@
 Given the head of a linked list and an integer val, remove all the nodes of the linked list
 that has Node.val == val, and return the new head.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val){
+
+}
