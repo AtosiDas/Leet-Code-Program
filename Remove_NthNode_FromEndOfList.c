379e625da8b2fd39e398c3314461e43e7f41126f
Atosi <atosidas44@gmail.com>
2023-07-18 12:26:02 +0530

    Solved

--- Remove_NthNode_FromEndOfList.c
+++ Remove_NthNode_FromEndOfList.c
@@ -7,5 +7,28 @@ Given the head of a linked list, remove the nth node from the end of the list an
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
-
+    struct ListNode* temp = head;
+    int count = 0;
+    while(temp != NULL){
+        count++;
+        temp = temp -> next;
+    }
+    temp =  head;
+    if(count - n != 0){
+        for(int i = 1; i < count-n; i++){
+            temp = temp->next;
+        }
+        if(temp->next == NULL){
+            head = NULL;
+            return head;
+        }
+        else{
+            temp -> next =temp->next->next;
+            return head;
+        }
+    }
+    else{
+        head = temp->next;
+        return head;
+    }
 }
