fe5dfc5c3802da9dfef4c9829b8d3b3134dbb860
Atosi <atosidas44@gmail.com>
2023-07-18 12:24:23 +0530

    Initial Statement

--- Remove_NthNode_FromEndOfList.c
+++ Remove_NthNode_FromEndOfList.c
@@ -1,1 +1,11 @@
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+
+}
