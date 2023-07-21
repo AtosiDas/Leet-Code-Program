5076ac0421e80f6e2b2aaca86827cdee45a2a059
Atosi <atosidas44@gmail.com>
2023-07-21 17:29:39 +0530

    Solved

--- MoveZores.c
+++ MoveZores.c
@@ -4,5 +4,18 @@ of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 
 void moveZeroes(int* nums, int numsSize){
-
+    int swap;
+    for(int i = 0;i < numsSize; i++){
+        if(nums[i] == 0){
+            for(int j = i+1; j < numsSize; j++){
+                if(nums[j] != 0){
+                    swap = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = swap;
+                    break;
+                }
+            }
+        }
+    }
+    return nums;
 }
