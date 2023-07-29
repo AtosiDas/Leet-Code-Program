df8a0d61ccd3bc3997a21d91e343a6aaadcecdcb
Atosi <atosidas44@gmail.com>
2023-07-29 17:20:13 +0530

    Initial lines

--- LongestContinuousIncreasingSubsequesce.js
+++ LongestContinuousIncreasingSubsequesce.js
@@ -4,3 +4,11 @@ subsequence (i.e. subarray). The subsequence must be strictly increasing.
 A continuous increasing subsequence is defined by two indices l and r (l < r) such that
 it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and
 for each l <= i < r, nums[i] < nums[i + 1].
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+
+};
